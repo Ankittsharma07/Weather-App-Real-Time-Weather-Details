a26b2d9d3b2dca9e7e28f4359b3332cc72ed5cea
Rk <ops64970@gmail.com>
2026-01-10 11:57:12 +0530

    fixing api

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const weatherIcons = {
     "Clear": "./weather-icon/Clear.png"
 };
 
-
+const API_KEY = process.env.API_KEY;
 
 function checkWeather(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
